e0de2f1b75e92e3aea0198ce2dc061e1198a0794
christa-ann <88717435+christa-ann@users.noreply.github.com>
2021-08-25 21:46:11 +0530

    Update footer.php

--- theme/footer.php
+++ theme/footer.php
@@ -2,7 +2,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-                        © <?php echo date('Y'); ?> DyaDni - Developed by <a href="https://rejola.com/">REJOLA IT SERVICES</a>.
+                        © <?php echo date('Y'); ?> PrimeIndia - Developed by <a href="https://rejola.com/">REJOLA IT SERVICES</a>.
                     </div>
                 </div>
             </div>
