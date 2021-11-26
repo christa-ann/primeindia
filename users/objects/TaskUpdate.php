9fa7c350f987f4cf2f6d7839373f0dbb9056aa09
christa-ann <88717435+christa-ann@users.noreply.github.com>
2021-11-26 13:25:23 +0530

    Update TaskUpdate.php

--- users/objects/TaskUpdate.php
+++ users/objects/TaskUpdate.php
@@ -184,7 +184,7 @@ class TaskUpdate{
       //echo "SELECT * FROM users WHERE active = '{$active}' AND rights < '100'";exit();
     $list = "<div class=\"box-body table-responsive\">
 
-                            <table id=\"datatable\" class=\"table table-striped table-bordered dt-responsive nowrap\" style=\"border-collapse: collapse; border-spacing: 0; width: 100%;\"><thead>
+                            <table id=\"\" class=\"table table-striped table-bordered dt-responsive nowrap\" style=\"border-collapse: collapse; border-spacing: 0; width: 100%;\"><thead>
           <tr>
             <th>Sl No</th>
             <th>Updates</th>
